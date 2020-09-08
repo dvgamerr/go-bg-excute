79603f3fce77975f20eb1f89dff88de9b46d4b29
Kananek Thongkam <info.dvgamer@gmail.com>
2020-09-08 23:47:02 +0700

    update name

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	fmt.Println(os.Args[0])
+	if os.Args[0] == "" {
+		return
+	}
+	path := filepath.Dir(os.Args[1])
+	file := filepath.Base(os.Args[1])
+	output := path + "\\" + strings.TrimSuffix(file, filepath.Ext(file)) + "_waifu2x.png"
 
 	// waifu2x-ncnn-vulkan_waifu2xEX -i "C:\Users\dvgamerr\Downloads\83794835_p001_.jpg" -o "C:\Users\dvgamerr\Downloads\83794835_p001_jpg.png" -f png
-	cmd := exec.Command("C:\\tools\\waifu2x-gui\\waifu2x-extension-gui\\waifu2x-ncnn-vulkan\\waifu2x-ncnn-vulkan_waifu2xEX.exe", "-i", os.Args[0], "-o", os.Args[0], "-f", "png")
-	err := cmd.Start()
-	fmt.Println(err)
+	cmd := exec.Command("C:\\tools\\waifu2x-gui\\waifu2x-extension-gui\\waifu2x-ncnn-vulkan\\waifu2x-ncnn-vulkan_waifu2xEX.exe", "-i", os.Args[1], "-o", output, "-f", "png")
+	cmd.Start()
 
 	// cmd.Stdin=c;
 	//cmd.Stdout= c;
